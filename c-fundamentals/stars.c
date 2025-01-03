34985becfaed4aeb45d50226e3964ef667f11377
rwx-700 <makonduchiyesu@gmail.com>
2025-01-03 21:29:47 +0200

    I don't know why I did this, but I have to C

---
+++ c-fundamentals/stars.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * Prints stars in a special
+ */
+int main(void)
+{
+    printf("          *\n");
+    printf("         *\n");
+    printf("        *\n");
+    printf(" *     *\n");
+    printf("   *  *\n");
+    printf("     *\n");
+
+    return 0;
+}
